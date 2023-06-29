73553907f59e0c498c2f2e4b698305939e2c34aa
Samuel Yohannes <127985871+yosami14@users.noreply.github.com>
2023-06-29 21:33:36 +0300

    minor fix

--- index.js
+++ index.js
@@ -1,44 +1,38 @@
+function displayInputs() {
+  var firstName = document.getElementById("firstName").value;
+  var email = document.getElementById("email").value;
+  var website = document.getElementById("website").value;
+  var imageLink = document.getElementById("imageLink").value;
+  var gender = document.querySelector('input[name="gender"]:checked');
+  var genderValue = gender ? gender.getAttribute("value") : '';
 
-  function displayInputs() {
-    var firstName = document.getElementById("firstName").value;
-    var email = document.getElementById("email").value;
-    var website = document.getElementById("website").value;
-    var imageLink = document.getElementById("imageLink").value;
-    var gender = document.querySelector('input[name="gender"]:checked');
-    var genderValue = gender ? gender.value : '';
-
-    var skills = document.querySelectorAll('input[name="skill"]:checked');
-    var selectedSkills = "";
-    for (var i = 0; i < skills.length; i++) {
-      selectedSkills += skills[i].value + ", ";
-    }
-    selectedSkills = selectedSkills.slice(0, -2); // Remove trailing comma and space
-
-    var displayDiv = document.querySelector(".display");
-    // var newImg = document.createElement("img");
-    // newImg.setAttribute('src',imageLink)
-    // newImg.classList.add('myImg')
-    var newDiv = document.createElement("div");
-    newDiv.className = "enrolledClass";
-    newDiv.innerHTML = `
-    <img src=" ${imageLink}" >
-      <p>Name: ${firstName}</p>
-      <p>Email: ${email}</p>
-      Website link: <a href="${website}">${website}</a>
-      
-      <p>Gender: ${genderValue}</p>
-      <p>Skills: ${selectedSkills}</p>
-    `;
-
-    displayDiv.appendChild(newDiv);
-    // newDiv.appendChild(newImg)
+  var skills = document.querySelectorAll('input[name="skill"]:checked');
+  var selectedSkills = "";
+  for (var i = 0; i < skills.length; i++) {
+    selectedSkills += skills[i].value + ", ";
   }
+  selectedSkills = selectedSkills.slice(0, -2); 
+  var displayDiv = document.querySelector(".display");
 
-  function clr() {
-    var displayDiv = document.querySelector(".display");
-    displayDiv.innerHTML = "";
-  }
+  var newDiv = document.createElement("div");
+  newDiv.className = "enrolledClass";
+  newDiv.innerHTML = `
+    <img src="${imageLink}">
+    <p>Name: ${firstName}</p>
+    <p>Email: ${email}</p>
+    Website link: <a href="${website}">${website}</a>
+    
+    <p>Gender: ${genderValue}</p>
+    <p>Skills: ${selectedSkills}</p>
+  `;
 
-  var enrollBtn = document.getElementById("enroll");
-  enrollBtn.addEventListener("click", displayInputs);
+  displayDiv.appendChild(newDiv);
+}
 
+function clr() {
+  var displayDiv = document.querySelector(".display");
+  displayDiv.innerHTML = "";
+}
+
+var enrollBtn = document.getElementById("enroll");
+enrollBtn.addEventListener("click", displayInputs);
